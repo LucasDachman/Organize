446e92bfb4b54c7af28864a95fa1c79344f3298d
Lucas Dachman <lucas.dachman@gmail.com>
2016-06-12 00:46:41 -0500

    worked on notifications. pressing notif now leads you to a window to view/edit the notif corrisponding to the note

---
+++ app/src/main/java/com/lucas_dachman/organize/NotificationHandlerFragment.java
@@ -0,0 +1,39 @@
+package com.lucas_dachman.organize;
+
+
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NotificationHandlerFragment extends Fragment {
+
+
+    public NotificationHandlerFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
